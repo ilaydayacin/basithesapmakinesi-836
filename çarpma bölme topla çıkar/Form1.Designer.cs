2a360ac4f567376f773e793e88e8e298c49577d8
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-09 14:37:39 +0300

    Add files via upload

---
+++ çarpma bölme topla çıkar/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace çarpma_bölme_topla_çıkar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.rbTopla = new System.Windows.Forms.RadioButton();
+            this.rbCikar = new System.Windows.Forms.RadioButton();
+            this.rbCarp = new System.Windows.Forms.RadioButton();
+            this.rbBol = new System.Windows.Forms.RadioButton();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Sayı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Sayı";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.txtSayi1.Location = new System.Drawing.Point(89, 35);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(173, 20);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.txtSayi2.Location = new System.Drawing.Point(89, 77);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(173, 20);
+            this.txtSayi2.TabIndex = 3;
+            // 
+            // rbTopla
+            // 
+            this.rbTopla.AutoSize = true;
+            this.rbTopla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.rbTopla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbTopla.Location = new System.Drawing.Point(17, 146);
+            this.rbTopla.Name = "rbTopla";
+            this.rbTopla.Size = new System.Drawing.Size(79, 24);
+            this.rbTopla.TabIndex = 4;
+            this.rbTopla.TabStop = true;
+            this.rbTopla.Text = "Toplam";
+            this.rbTopla.UseVisualStyleBackColor = false;
+            // 
+            // rbCikar
+            // 
+            this.rbCikar.AutoSize = true;
+            this.rbCikar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.rbCikar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbCikar.Location = new System.Drawing.Point(160, 146);
+            this.rbCikar.Name = "rbCikar";
+            this.rbCikar.Size = new System.Drawing.Size(85, 24);
+            this.rbCikar.TabIndex = 5;
+            this.rbCikar.TabStop = true;
+            this.rbCikar.Text = "Çıkarma";
+            this.rbCikar.UseVisualStyleBackColor = false;
+            // 
+            // rbCarp
+            // 
+            this.rbCarp.AutoSize = true;
+            this.rbCarp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.rbCarp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbCarp.Location = new System.Drawing.Point(17, 199);
+            this.rbCarp.Name = "rbCarp";
+            this.rbCarp.Size = new System.Drawing.Size(83, 24);
+            this.rbCarp.TabIndex = 6;
+            this.rbCarp.TabStop = true;
+            this.rbCarp.Text = "Çarpma";
+            this.rbCarp.UseVisualStyleBackColor = false;
+            // 
+            // rbBol
+            // 
+            this.rbBol.AutoSize = true;
+            this.rbBol.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.rbBol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbBol.Location = new System.Drawing.Point(160, 199);
+            this.rbBol.Name = "rbBol";
+            this.rbBol.Size = new System.Drawing.Size(72, 24);
+            this.rbBol.TabIndex = 7;
+            this.rbBol.TabStop = true;
+            this.rbBol.Text = "Bölme";
+            this.rbBol.UseVisualStyleBackColor = false;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(17, 265);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(228, 95);
+            this.btnHesapla.TabIndex = 8;
+            this.btnHesapla.Text = "Hesaplama";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(336, 170);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(85, 25);
+            this.lblSonuc.TabIndex = 9;
+            this.lblSonuc.Text = "Sonuç: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(660, 397);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.rbBol);
+            this.Controls.Add(this.rbCarp);
+            this.Controls.Add(this.rbCikar);
+            this.Controls.Add(this.rbTopla);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.RadioButton rbTopla;
+        private System.Windows.Forms.RadioButton rbCikar;
+        private System.Windows.Forms.RadioButton rbCarp;
+        private System.Windows.Forms.RadioButton rbBol;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ çarpma bölme topla çıkar/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace çarpma_bölme_topla_çıkar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            Double sayi1 = Convert.ToDouble(txtSayi1.Text);
+            Double sayi2 = Convert.ToDouble(txtSayi2.Text);
+
+            if (rbTopla.Checked)
+            {
+                lblSonuc.Text = "Sonuç: " + Convert.ToString(sayi1 + sayi2);
+            }
+            else if (rbCikar.Checked)
+            {
+                lblSonuc.Text = "Sonuç: " + Convert.ToString(sayi1 - sayi2);
+            }
+            else if (rbCarp.Checked)
+            {
+                lblSonuc.Text = "Sonuç: " + Convert.ToString(sayi1 * sayi2);
+            }
+
+            else if (rbBol.Checked)
+            {
+                lblSonuc.Text = "Sonuç: " + Convert.ToString(sayi1 / sayi2);
+            }
+        }
+    }
+}
